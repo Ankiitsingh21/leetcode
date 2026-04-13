9f7ff14690038e71b699a3c83b2b7f399b55609b
Ankit Singh <126193019+Ankiitsingh21@users.noreply.github.com>
2026-04-14 05:12:54 +0530

    Time: 0 ms (100%), Space: 9.6 MB (100%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -9,18 +9,21 @@ public:
         if(n==1){
             return nums[0];
         }
-        int prev=nums[0];
-        int prev2=0;
+        // if(n==2){
+        //     return max(nums[0],nums[1]);
+        // }
+        vector<int> dp(n,0);
+        dp[0]=nums[0];
+        int neg=0;
         for(int i=1;i<n;i++){
             int take=nums[i];
             if(i>1){
-                take+=prev2;
+                take+=dp[i-2];
             }
-            int nt=0+prev;
-            int curri=max(take,nt);
-            prev2=prev;
-            prev=curri;
+            int nt=0+dp[i-1];
+            cout<<take<<" "<<nt<<endl;
+            dp[i]=max(take,nt);
         }
-        return prev;
+        return dp[n-1];
     }
 };
