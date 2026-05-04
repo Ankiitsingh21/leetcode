c8ce35ca4142c67dc555133d74307f56b67f92cd
Ankit Singh <126193019+Ankiitsingh21@users.noreply.github.com>
2026-05-04 07:55:21 +0530

    Time: 0 ms (100%), Space: 13.8 MB (96.87%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        long long xorr=0;
+        for(int num:nums){
+            xorr=xorr^num;
+        }
+        xorr=xorr & (-xorr);
+        int a=0,b=0;
+        for(int num:nums){
+            if(xorr&num){
+                a=a^num;
+            }else{
+                b=b^num;
+            }
+        }
+        return {a,b};
+    }
+};
