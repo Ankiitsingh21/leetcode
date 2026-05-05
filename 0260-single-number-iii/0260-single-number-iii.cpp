e423c89d12d2e56e22a6e9faeb9f1626db85353a
Ankit Singh <126193019+Ankiitsingh21@users.noreply.github.com>
2026-05-05 08:12:25 +0530

    Time: 0 ms (100%), Space: 13.9 MB (54.57%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
         long long xorr=0;
-        for(int num:nums){
+        for(int &num:nums){
             xorr=xorr^num;
         }
-        xorr=xorr & (-xorr);
+        xorr=xorr&(-xorr);
         int a=0,b=0;
         for(int num:nums){
             if(xorr&num){
