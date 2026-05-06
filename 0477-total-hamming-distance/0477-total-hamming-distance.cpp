4b674cde57076fe88430bf92c5eb65035d0855f9
Ankit Singh <126193019+Ankiitsingh21@users.noreply.github.com>
2026-05-06 11:48:22 +0530

    Time: 2 ms (96.51%), Space: 22.8 MB (79.39%) - LeetHub

--- 0477-total-hamming-distance/0477-total-hamming-distance.cpp
+++ 0477-total-hamming-distance/0477-total-hamming-distance.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int totalHammingDistance(vector<int>& nums) {
-        int sum=0;
-        int ones=0;
-        for(int i=0;i<32;i++){
-             ones=0;
-            for(int &num:nums){
-                int bit=(num>>i)&1;
-                if(bit)ones++;
+        int n = nums.size();
+        int sum = 0;
+
+        for(int i = 0; i < 32; i++) {
+            int ones = 0;
+
+            for(int num : nums) {
+                ones += (num >> i) & 1;
             }
-            sum+=(ones*(nums.size()-ones));
+
+            sum += ones * (n - ones);
         }
+
         return sum;
     }
 };
