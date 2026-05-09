7065dc3b245c942e4ab75d4085c5ac469681a3c2
Ankit Singh <126193019+Ankiitsingh21@users.noreply.github.com>
2026-05-09 06:26:18 +0530

    Time: 10 ms (49.87%), Space: 25.9 MB (83.87%) - LeetHub

--- 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.cpp
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.cpp
@@ -1,32 +1,16 @@
 class Solution {
-    bool check(string s, string b) {
-        for (int i = 0; i < s.size(); i++) {
-            for (int j = 0; j < b.size(); j++) {
-                if (s[i] == b[j])
-                    return false;
-            }
-        }
-        return true;
-    }
-
 public:
     int maxProduct(vector<string>& words) {
-        vector<int> mask(words.size(),0);
-        for(int i=0;i<words.size();i++){
-            for(char ch:words[i]){
-                mask[i]|=(1<<(ch-'a'));
-            }
-        }
-        int maxi = 0;
-        for (int i = 0; i < words.size() - 1; i++) {
-            string s=words[i];
-            for (int j = i + 1; j < words.size(); j++) {
-                string b=words[j];
-                if ((mask[i]&mask[j])==0) {
-                    maxi = max(maxi, (int)(s.size() * b.size()));
-                }
-            }
-        }
-        return maxi;
+        int n = size(words), ans = 0;
+        vector<int> mask(n);
+
+       for(int i = 0; i < n; i++) {
+		for(auto& ch : words[i])           
+			mask[i] |= 1 << (ch - 'a');     // hash the word
+		for(int j = 0; j < i; j++)
+			if((mask[i] & mask[j]) == 0)    // no common set bit in the hash of words[i] and words[j]
+				ans = max(ans, int(size(words[i]) * size(words[j])));
+	}   
+	return ans;
     }
 };
