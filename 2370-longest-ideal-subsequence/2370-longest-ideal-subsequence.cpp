8330e902221fb8013b102ec47c709e630577db20
Ankit Singh <126193019+Ankiitsingh21@users.noreply.github.com>
2026-05-13 08:08:00 +0530

    Time: 143 ms (36.41%), Space: 13.5 MB (40.11%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
@@ -1,21 +1,40 @@
 class Solution {
-    int solve(int index,string &s,int k,int last,vector<vector<int>>&dp){
-        if(index==s.size()) return 0;
-        if(dp[index][last+1]!=-1) return dp[index][last+1];
-        //skipp
-        int skip=solve(index+1,s,k,last,dp);
+    int solve(int index, string& s, int k, int last, vector<vector<int>>& dp) {
+        if (index == s.size())
+            return 0;
+        if (dp[index][last + 1] != -1)
+            return dp[index][last + 1];
+        // skipp
+        int skip = solve(index + 1, s, k, last, dp);
 
-        //fill
-        int fill=0;
-        if(last==-1 || abs(s[index]-'a'-last)<=k){
-            fill=1+solve(index+1,s,k,s[index]-'a',dp);
+        // fill
+        int fill = 0;
+        if (last == -1 || abs(s[index] - 'a' - last) <= k) {
+            fill = 1 + solve(index + 1, s, k, s[index] - 'a', dp);
         }
 
-        return dp[index][last+1]= max(skip,fill);
+        return dp[index][last + 1] = max(skip, fill);
     }
+
 public:
     int longestIdealString(string s, int k) {
-        vector<vector<int>>dp(s.size(),vector<int>(27,-1));
-        return solve(0,s,k,-1,dp);
+        // vector<vector<int>> dp(s.size()+1, vector<int>(27, 0));
+        // return solve(0,s,k,-1,dp);
+        vector<int> next(27,0),curr(27,0);
+        for (int index = s.size() - 1; index >= 0; index--) {
+            for (int last = -1; last <= 25; last++) {
+                int skip = next[last+1];
+
+                // fill
+                int fill = 0;
+                if (last == -1 || abs(s[index] - 'a' - last) <= k) {
+                    fill = 1 + next[s[index] - 'a'+1];
+                }
+
+                curr[last + 1] = max(skip, fill);
+            }
+            next=curr;
+        }
+        return next[0];
     }
 };
