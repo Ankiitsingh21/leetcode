d00dcc70a7831b70814cf5e9d1cea52bc8ef932a
Ankit Singh <126193019+Ankiitsingh21@users.noreply.github.com>
2026-05-24 06:44:50 +0530

    Time: 111 ms (5.4%), Space: 211.1 MB (10.91%) - LeetHub

--- 3938-maximum-path-intersection-sum-in-a-grid/3938-maximum-path-intersection-sum-in-a-grid.cpp
+++ 3938-maximum-path-intersection-sum-in-a-grid/3938-maximum-path-intersection-sum-in-a-grid.cpp
@@ -1,36 +1,61 @@
 class Solution {
 public:
     int maxScore(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        long long ans=LLONG_MIN;
-        for(int i=1;i+1<m;++i){
-            for(int j=1;j+1<n;++j){
-                ans=max(ans,(long long) grid[i][j]);
+
+        int m = grid.size();
+        int n = grid[0].size();
+
+        long long ans = LLONG_MIN;
+
+        // single cell
+        for (int i = 1; i < m - 1; i++) {
+
+            for (int j = 1; j < n - 1; j++) {
+
+                ans = max(ans, (long long)grid[i][j]);
             }
         }
-        for(int i=0;i<m;++i){
-            vector<long long> pref(n+1,0);
-            for(int j=0;j<n;++j){
-                pref[j+1]=pref[j]+grid[i][j];
+
+        // row wise
+        for (int i = 0; i < m; i++) {
+
+            vector<long long> prefix(n + 1, 0);
+
+            for (int j = 0; j < n; j++) {
+
+                prefix[j + 1] = prefix[j] + grid[i][j];
             }
-            long long minPref=0;
-            for(int r=2;r<=n;r++){
-                minPref=min(minPref,pref[r-2]);
-                ans=max(ans,pref[r]-minPref);
-            }
-        }
-        for(int j=0;j<n;++j){
-            vector<long long> pref(m+1,0);
-            for(int i=0;i<m;i++){
-                pref[i+1]=pref[i]+grid[i][j];
-            }
-            long long minPref=0;
-            for(int r=2;r<=m;++r){
-                minPref=min(minPref,pref[r-2]);
-                ans=max(ans,pref[r]-minPref);
+
+            long long mini = 0;
+
+            for (int r = 2; r <= n; r++) {
+
+                mini = min(mini, prefix[r - 2]);
+
+                ans = max(ans, prefix[r] - mini);
             }
         }
+
+        // column wise
+        for (int j = 0; j < n; j++) {
+
+            vector<long long> prefix(m + 1, 0);
+
+            for (int i = 0; i < m; i++) {
+
+                prefix[i + 1] = prefix[i] + grid[i][j];
+            }
+
+            long long mini = 0;
+
+            for (int r = 2; r <= m; r++) {
+
+                mini = min(mini, prefix[r - 2]);
+
+                ans = max(ans, prefix[r] - mini);
+            }
+        }
+
         return (int)ans;
     }
 };
