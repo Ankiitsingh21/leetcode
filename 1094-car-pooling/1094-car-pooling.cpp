0adc82a0ea0da716dea3ac07df7dcd5773b9f4e1
Ankit Singh <126193019+Ankiitsingh21@users.noreply.github.com>
2026-05-28 05:26:48 +0530

    Time: 0 ms (100%), Space: 14.3 MB (69.39%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        vector<pair<int,int>> event;
+        vector<pair<int,int>> eve;
         for(int i=0;i<trips.size();i++){
-            event.push_back({trips[i][1],trips[i][0]});
-            event.push_back({trips[i][2],-trips[i][0]});
+            int c=trips[i][0];
+            int u=trips[i][1];
+            int v=trips[i][2];
+            eve.push_back({u,c});
+            eve.push_back({v,-c});
         }
-        sort(event.begin(),event.end());
+        sort(eve.begin(),eve.end());
         int curr=0;
-        for(auto &e:event){
-            curr+=e.second;
+        for(int i=0;i<eve.size();i++){
+            curr+=eve[i].second;
             if(curr>capacity) return false;
         }
         return true;
