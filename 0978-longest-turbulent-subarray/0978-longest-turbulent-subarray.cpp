7abbb4bcb4bd41ea69b462a649f3e9b5dfb9bcd6
Ankit Singh <126193019+Ankiitsingh21@users.noreply.github.com>
2026-05-31 04:35:45 +0530

    Time: 0 ms (100%), Space: 44.2 MB (27.22%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.cpp
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.cpp
@@ -5,7 +5,8 @@ class Solution {
 
         if (sign == 0) {
             int diff = arr[index] - arr[index - 1];
-            if(diff==0) return 0;
+            if (diff == 0)
+                return 0;
             if (diff < 0) {
                 return 1 + solve(index + 1, arr, 1);
             } else {
@@ -18,14 +19,44 @@ class Solution {
         }
         return 0;
     }
+    // int slideSolve(int index,vector<int> &arr,int sign){
+    //     if(index==arr.size()) return 0;
+
+    //     int l=0,r=0;
+    //     int ans=1;
+    //     while(r<arr.size()){
+    //         while(l)
+    //         ans=max(ans,r-l+1);
+    //         r++;
+    //     }
+
+    //     return ans;
+    // }
 
 public:
     int maxTurbulenceSize(vector<int>& arr) {
-        if(arr.size()==1) return 1;
+        // if(arr.size()==1) return 1;
+        // int ans = 1;
+        // for (int i = 0; i < arr.size()-1; i++) {
+        //     // cout<<arr[i]<<" "<<i<<" "<<endl;
+        //     ans = max(ans,  1+slideSolve(i + 1, arr, 0));
+        // }
+        // return ans;
+
+        int l = 0, r = 1;
         int ans = 1;
-        for (int i = 0; i < arr.size()-1; i++) {
-            // cout<<arr[i]<<" "<<i<<" "<<endl;
-            ans = max(ans,  1+solve(i + 1, arr, 0));
+        while (r < arr.size()) {
+            int curr = arr[r] - arr[r - 1];
+
+            if (curr == 0) {
+                l = r;
+            } else if (r == arr.size() - 1 ||
+                       (long long)curr * (arr[r + 1] - arr[r]) >= 0) {
+
+                ans = max(ans, r - l + 1);
+                l = r;
+            }
+            r++;
         }
         return ans;
     }
