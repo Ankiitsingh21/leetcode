12cabd4f7f5f23fc4ae3f199c592b1c7df1747e5
Ankit Singh <126193019+Ankiitsingh21@users.noreply.github.com>
2026-05-31 17:27:38 +0530

    Time: 573 ms (10.8%), Space: 296.6 MB (11.28%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,37 +1,20 @@
 class Solution {
-    int solve(int index, int lindex, vector<int>& nums,
-              vector<vector<int>>& dp) {
+    int solve(int index, vector<int>& nums, int lindex,vector<vector<int>> &dp) {
         if (index == nums.size())
             return 0;
 
-        if (dp[index][lindex + 1] != -1)
-            return dp[index][lindex + 1];
+        if(dp[index][lindex+1]!=-1 )  return dp[index][lindex+1]; 
+        int nottake = solve(index + 1, nums,lindex,dp);
         int take = 0;
         if (lindex == -1 || nums[index] > nums[lindex]) {
-            take = solve(index + 1, index, nums, dp) + 1;
+            take = 1 + solve(index + 1, nums, index,dp);
         }
-        int nottake = solve(index + 1, lindex, nums, dp);
-
-        return dp[index][lindex + 1] = max(nottake, take);
+        return dp[index][lindex+1]= max(take, nottake);
     }
 
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size() + 1,
-                               vector<int>(nums.size() + 1, -1));
-        return solve(0, -1, nums, dp);
-
-        for (int index = nums.size() - 1; index >= 0; index--) {
-            for (int lindex = -1; lindex < index; lindex++) {
-                int take = 0;
-                if (lindex == -1 || nums[index] > nums[lindex]) {
-                    take = dp[index + 1][index + 1] + 1;
-                }
-                int nottake = dp[index + 1][lindex + 1];
-
-                dp[index][lindex + 1] = max(nottake, take);
-            }
-        }
-        return dp[0][0];
+    int lengthOfLIS(vector<int>& nums) { 
+        vector<vector<int>> dp(nums.size(),vector<int>(nums.size()+1,-1));
+        return solve(0, nums, -1,dp); 
     }
 };
