541b49295a162cdb6cb0fc1d6c2b62a9ea9e1500
Ankit Singh <126193019+Ankiitsingh21@users.noreply.github.com>
2026-06-04 08:02:17 +0530

    Time: 0 ms (100%), Space: 23 MB (52.25%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -3,24 +3,18 @@ public:
     int candy(vector<int>& ratings) {
         int n=ratings.size();
         vector<int> prefix(n,1);
-        prefix[0]=1;
-        for(int i=1;i<prefix.size();i++){
+        for(int i=1;i<n;i++){
             if(ratings[i]>ratings[i-1]){
                 prefix[i]=prefix[i-1]+1;
             }
         }
-        int sum=0;
-        for(int i=prefix.size()-2;i>=0;i--){
+        int sum=prefix[n-1];
+        for(int i=n-2;i>=0;i--){
             if(ratings[i]>ratings[i+1] && prefix[i]<=prefix[i+1]){
                 prefix[i]=prefix[i+1]+1;
             }
             sum+=prefix[i];
         }
-        // vector<int> suffix(ratings.size(),1);
-        // for(int i=0;i<prefix.size();i++){
-        //     sum+=prefix[i];
-        //     // cout<<prefix[i]<<" ";
-        // }
-        return sum+prefix[n-1];
+        return sum;
     }
 };
