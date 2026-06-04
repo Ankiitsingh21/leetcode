10072220fd2ead4382af0389e2db13124d5c243b
Ankit Singh <126193019+Ankiitsingh21@users.noreply.github.com>
2026-06-04 08:45:49 +0530

    Time: 0 ms (100%), Space: 9.6 MB (89.38%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,18 +1,20 @@
 class Solution {
+    vector<int> solve(int row){
+        int ans=1;
+        vector<int> anss;
+        anss.push_back(1);
+        for(int i=1;i<=row;i++){
+            ans=ans*(row-i+1);
+            ans=ans/i;
+            anss.push_back(ans);
+        }
+        return anss;
+    }
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ans;
-        int col=1;
-        for(int i=1;i<=numRows;i++){
-            vector<int> res;
-            res.push_back(1);
-            for(int j=2;j<i;j++){
-                int val=ans[i-2][j-2]+ans[i-2][j-1];
-                res.push_back(val);
-            }
-            if(i>1) res.push_back(1);
-            ans.push_back(res);
-            // cout<<endl;
+        for(int i=0;i<numRows;i++){
+            ans.push_back(solve(i));
         }
         return ans;
     }
