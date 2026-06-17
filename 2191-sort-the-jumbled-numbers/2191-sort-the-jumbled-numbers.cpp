a828e9b34b26874c970ddfb833da234e5edec0d4
Ankit Singh <126193019+Ankiitsingh21@users.noreply.github.com>
2026-06-17 08:09:40 +0530

    Time: 81 ms (72.6%), Space: 88.5 MB (77.4%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
     vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        for(int i=0;i<nums.size();i++){
-            int num=nums[i];
-            vector<int> arr;
-            string s=to_string(num);
-            for(char c:s){
-                arr.push_back(mapping[c-'0']);
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       greater<pair<int, int>>>
+            pq;
+        for (int i = 0; i < nums.size(); i++) {
+            int num = nums[i];
+            int place = 1;
+            int n = 0;
+            if (num == 0) {
+                n = mapping[0];
+            } else {
+
+                while (num > 0) {
+                    int digit = num % 10;
+                    n += mapping[digit] * place;
+                    place *= 10;
+                    num /= 10;
+                }
             }
-            int n=0;
-            for(int i=0;i<arr.size();i++){
-                n=n*10+arr[i];
-            }
-            pq.push({n,i});            
+            pq.push({n, i});
         }
         vector<int> ans;
-        while(!pq.empty()){
+        while (!pq.empty()) {
             // cout<<pq.top().first<<" "<<pq.top().second<<" ";
             ans.push_back(nums[pq.top().second]);
             pq.pop();
