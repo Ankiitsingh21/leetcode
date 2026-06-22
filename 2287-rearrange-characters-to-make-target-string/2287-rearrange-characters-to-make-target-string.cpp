dfcbac1201b67735ebdcae91284f06154d3085d6
Ankit Singh <126193019+Ankiitsingh21@users.noreply.github.com>
2026-06-22 05:59:56 +0530

    Time: 0 ms (100%), Space: 8.8 MB (12.43%) - LeetHub

---
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int rearrangeCharacters(string s, string target) {
+        unordered_map<char,int> mpp;
+        for(char c:s){
+            mpp[c]++;
+        }
+        unordered_map<char,int> freq;
+        for(char c:target){
+            freq[c]++;
+        }
+        int ans=INT_MAX;
+        for(auto it:freq){
+            ans=min(ans,mpp[it.first]/freq[it.first]);
+        }
+        return ans;
+    }
+};
