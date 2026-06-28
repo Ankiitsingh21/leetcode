53096ccc0ddf35e236157f173197a88c4771de82
Ankit Singh <126193019+Ankiitsingh21@users.noreply.github.com>
2026-06-28 07:21:13 +0530

    Time: 0 ms (100%), Space: 77.2 MB (54.23%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.cpp
+++ 1701-average-waiting-time/1701-average-waiting-time.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& customers) {
-        // sort(customers.begin(), customers.end());
-        // for(int i=0;i<customers.size();i++){
-        //     cout<<customers[i][0]<<" "<<customers[i][1]<<" "<<endl;
-        // }
         double totalWt = 0;
         int time = customers[0][0];
         for (int i = 0; i < customers.size(); i++) {
-            if (customers[i][0] > time) {
-                time += (customers[i][0] - time);
-            }
-             time += customers[i][1];
+            time = max(time, customers[i][0]);
+            time += customers[i][1];
             totalWt += (time - customers[i][0]);
-            // cout<<totalWt<<" "<<wait<<" "<<customers[i][0]<<" "<<i<<" "<<endl;
-            // time += customers[i][1];
         }
-        cout<<totalWt<<" ";
         return totalWt / customers.size();
     }
 };
