27f2ad6ca8c3efc286977b7cd83117122bfaaba3
Ankit Singh <126193019+Ankiitsingh21@users.noreply.github.com>
2026-07-03 08:31:49 +0530

    Time: 71 ms (88.52%), Space: 101.7 MB (74.36%) - LeetHub

---
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    void update(vector<int>& bits, int val, int id, int n) {
+        while (id <= n) {
+            bits[id] += val;
+            id += (id & -id);
+        }
+        return;
+    }
+    int query(vector<int>& bits, int id) {
+        int sum = 0;
+        while (id > 0) {
+            sum += bits[id];
+            id &= (id - 1);
+        }
+        return sum;
+    }
+
+public:
+    vector<int> countSmaller(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> bits(n + 1, 0);
+        vector<pair<int, int>> arr;
+        for (int i = 0; i < nums.size(); i++) {
+            arr.push_back({nums[i], i});
+        }
+        sort(arr.begin(), arr.end());
+        vector<int> ans(n, 0);
+        for (auto& [val, id] : arr) {
+            update(bits, 1, id+1, n);
+            ans[id] = query(bits,n) - query(bits,id + 1);
+        }
+        return ans;
+    }
+};
