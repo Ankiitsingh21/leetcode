4f47d3863936ac2b9c9fcb25318514659ea6a491
Ankit Singh <126193019+Ankiitsingh21@users.noreply.github.com>
2026-07-05 08:44:58 +0530

    Time: 107 ms (19.86%), Space: 90.2 MB (16.03%) - LeetHub

--- 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
+++ 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
@@ -2,74 +2,87 @@ class Solution {
 public:
     const int MOD = 1e9 + 7;
 
+    pair<int, int> dfs(
+        int i,
+        int j,
+        vector<string>& board,
+        vector<vector<int>>& score,
+        vector<vector<int>>& ways,
+        vector<vector<int>>& vis
+    ) {
+        // Out of bounds or obstacle
+        if (i < 0 || j < 0 || board[i][j] == 'X')
+            return {-1, 0};
+
+        // Reached E
+        if (i == 0 && j == 0)
+            return {0, 1};
+
+        // Already computed
+        if (vis[i][j])
+            return {score[i][j], ways[i][j]};
+
+        vis[i][j] = 1;
+
+        int best = -1;
+        long long cnt = 0;
+
+        pair<int, int> up = dfs(i - 1, j, board, score, ways, vis);
+        pair<int, int> left = dfs(i, j - 1, board, score, ways, vis);
+        pair<int, int> diag = dfs(i - 1, j - 1, board, score, ways, vis);
+
+        vector<pair<int, int>> prev;
+        prev.push_back(up);
+        prev.push_back(left);
+        prev.push_back(diag);
+
+        for (int k = 0; k < 3; k++) {
+
+            int currScore = prev[k].first;
+            int currWays = prev[k].second;
+
+            if (currScore == -1)
+                continue;
+
+            if (currScore > best) {
+                best = currScore;
+                cnt = currWays;
+            }
+            else if (currScore == best) {
+                cnt = (cnt + currWays) % MOD;
+            }
+        }
+
+        if (best == -1) {
+            score[i][j] = -1;
+            ways[i][j] = 0;
+            return {-1, 0};
+        }
+
+        int val = 0;
+
+        if (board[i][j] != 'S')
+            val = board[i][j] - '0';
+
+        score[i][j] = best + val;
+        ways[i][j] = cnt % MOD;
+
+        return {score[i][j], ways[i][j]};
+    }
+
     vector<int> pathsWithMaxScore(vector<string>& board) {
+
         int n = board.size();
 
         vector<vector<int>> score(n, vector<int>(n, -1));
         vector<vector<int>> ways(n, vector<int>(n, 0));
+        vector<vector<int>> vis(n, vector<int>(n, 0));
 
-        // Start from S
-        score[n - 1][n - 1] = 0;
-        ways[n - 1][n - 1] = 1;
+        pair<int, int> ans = dfs(n - 1, n - 1, board, score, ways, vis);
 
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-
-                if (board[i][j] == 'X')
-                    continue;
-
-                // Skip S because it is already initialized
-                if (i == n - 1 && j == n - 1)
-                    continue;
-
-                int best = -1;
-                long long cnt = 0;
-
-                // From below
-                if (i + 1 < n && score[i + 1][j] != -1) {
-                    if (score[i + 1][j] > best) {
-                        best = score[i + 1][j];
-                        cnt = ways[i + 1][j];
-                    } else if (score[i + 1][j] == best) {
-                        cnt = (cnt + ways[i + 1][j]) % MOD;
-                    }
-                }
-
-                // From right
-                if (j + 1 < n && score[i][j + 1] != -1) {
-                    if (score[i][j + 1] > best) {
-                        best = score[i][j + 1];
-                        cnt = ways[i][j + 1];
-                    } else if (score[i][j + 1] == best) {
-                        cnt = (cnt + ways[i][j + 1]) % MOD;
-                    }
-                }
-
-                // From diagonal
-                if (i + 1 < n && j + 1 < n && score[i + 1][j + 1] != -1) {
-                    if (score[i + 1][j + 1] > best) {
-                        best = score[i + 1][j + 1];
-                        cnt = ways[i + 1][j + 1];
-                    } else if (score[i + 1][j + 1] == best) {
-                        cnt = (cnt + ways[i + 1][j + 1]) % MOD;
-                    }
-                }
-
-                if (best == -1)
-                    continue;
-
-                score[i][j] = best;
-                ways[i][j] = cnt % MOD;
-
-                // Add current cell value
-                if (board[i][j] != 'E')
-                    score[i][j] += board[i][j] - '0';
-            }
-        }
-
-        if (ways[0][0] == 0)
+        if (ans.first == -1)
             return {0, 0};
 
-        return {score[0][0], ways[0][0]};
+        return {ans.first, ans.second};
     }
 };
