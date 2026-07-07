4dbfd7b260b006cec4e03e58845ca8b974447934
Ankit Singh <126193019+Ankiitsingh21@users.noreply.github.com>
2026-07-07 07:19:14 +0530

    Time: 62 ms (91.94%), Space: 102 MB (72.67%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
@@ -1,7 +1,7 @@
 class Solution {
-    void update(vector<int>& bits, int val, int id, int n) {
+    void update(vector<int>& bits, int id, int n) {
         while (id <= n) {
-            bits[id] += val;
+            bits[id] += 1;
             id += (id & -id);
         }
         return;
@@ -10,24 +10,23 @@ class Solution {
         int sum = 0;
         while (id > 0) {
             sum += bits[id];
-            id &= (id - 1);
+            id -= (id & -id);
         }
         return sum;
     }
 
 public:
     vector<int> countSmaller(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> bits(n + 1, 0);
         vector<pair<int, int>> arr;
         for (int i = 0; i < nums.size(); i++) {
             arr.push_back({nums[i], i});
         }
         sort(arr.begin(), arr.end());
-        vector<int> ans(n, 0);
-        for (auto& [val, id] : arr) {
-            update(bits, 1, id + 1, n);
-            ans[id] = query(bits, n) - query(bits, id + 1);
+        vector<int> ans(nums.size(), 0);
+        vector<int> bits(nums.size() + 1, 0);
+        for (auto& [el, id] : arr) {
+            update(bits, id + 1, nums.size());
+            ans[id] = query(bits, nums.size()) - query(bits, id + 1);
         }
         return ans;
     }
