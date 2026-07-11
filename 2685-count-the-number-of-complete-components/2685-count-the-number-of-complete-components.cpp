33ea9aadc904b87faf3d667380c6b3c1c06c7518
Ankit Singh <126193019+Ankiitsingh21@users.noreply.github.com>
2026-07-11 06:25:11 +0530

    Time: 20 ms (91.79%), Space: 128.1 MB (76.73%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -1,40 +1,59 @@
 class Solution {
-    pair<int,int> check(int node,vector<vector<int>> &adj,vector<bool> &vis){
-        vis[node]=true;
+    pair<int, int> check(int node, vector<vector<int>>& adj,
+                         vector<bool>& vis) {
+        vis[node] = true;
         queue<int> q;
         q.push(node);
-        int vertices=0,edges=0;
-        while(!q.empty()){
-            int top=q.front();
+        int vertices = 0, edges = 0;
+        while (!q.empty()) {
+            int top = q.front();
             q.pop();
             vertices++;
-            edges+=adj[top].size();
-            for(int &v:adj[top]){
-                if(!vis[v]){
-                    vis[v]=true;
+            edges += adj[top].size();
+            for (int& v : adj[top]) {
+                if (!vis[v]) {
+                    vis[v] = true;
                     q.push(v);
                 }
             }
         }
-        edges/=2;
-        return {vertices,edges};
+        edges /= 2;
+        return {vertices, edges};
     }
+
+    pair<int, int> solve(int node, vector<vector<int>>& adj,
+                         vector<bool>& vis) {
+        vis[node] = true;
+        int ver = 1;
+        int edges = adj[node].size();
+        for (int& v : adj[node]) {
+            if (!vis[v]) {
+                auto [ve, e] = solve(v, adj, vis);
+                ver += ve;
+                edges += e;
+            }
+        }
+        return {ver,edges};
+    }
+
 public:
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
         vector<vector<int>> adj(n);
-        for(int i=0;i<edges.size();i++){
-            int u=edges[i][0];
-            int v=edges[i][1];
+        for (int i = 0; i < edges.size(); i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        vector<bool> vis(n,false);
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                auto [ver,edge]=check(i,adj,vis);
-                int calc=(ver*(ver-1))/2;
-                if(edge==calc) cnt++;
+        vector<bool> vis(n, false);
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                auto [ver, edge] = solve(i, adj, vis);
+                edge/=2;
+                int calc = (ver * (ver - 1)) / 2;
+                if (edge == calc)
+                    cnt++;
                 // cnt++;
             }
         }
