a0284a33681db2d3615277d9646a41adf150386d
Ankit Singh <126193019+Ankiitsingh21@users.noreply.github.com>
2026-07-11 07:27:15 +0530

    Time: 95 ms (98.35%), Space: 138.8 MB (76.86%) - LeetHub

--- 3072-distribute-elements-into-two-arrays-ii/3072-distribute-elements-into-two-arrays-ii.cpp
+++ 3072-distribute-elements-into-two-arrays-ii/3072-distribute-elements-into-two-arrays-ii.cpp
@@ -1,50 +1,65 @@
-#include <ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds;
 class Solution {
-    typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag,
-                 tree_order_statistics_node_update>
-        ordered_set;
+    void update(int id, int n, vector<int>& bits) {
+        while (id <= n) {
+            bits[id] += 1;
+            id += id & (-id);
+        }
+        return;
+    }
+    int query(int id, vector<int>& bits) {
+        int sum = 0;
+        while (id > 0) {
+            sum += bits[id];
+            id -= id & (-id);
+        }
+        return sum;
+    }
 
 public:
     vector<int> resultArray(vector<int>& nums) {
-        vector<int> ans, arr1, arr2;
-        ordered_set st1, st2;
-        st1.insert({nums[0], 0});
-        st2.insert({nums[1], 0});
-
+        int n = nums.size();
+        vector<int> comp = nums;
+        sort(comp.begin(), comp.end());
+        comp.erase(unique(comp.begin(), comp.end()), comp.end());
+        int m = comp.size();
+        vector<int> bits1(m + 1), bits2(m + 1);
+        vector<int> arr1, arr2;
         arr1.push_back(nums[0]);
         arr2.push_back(nums[1]);
-        //    nums.erase(nums.begin());
-        //    nums.erase(nums.begin());
-        for (int i = 2; i < nums.size(); i++) {
-            int val = nums[i] + 1;
-            int id1 = st1.order_of_key({val, 0});
-            int id2 = st2.order_of_key({val, 0});
-            int sz1 = arr1.size() - id1;
-            int sz2 = arr2.size() - id2;
 
-            if (sz1 > sz2) {
-                arr1.push_back(val - 1);
-                st1.insert({val - 1, i});
-            } else if (sz1 < sz2) {
-                arr2.push_back(val - 1);
-                st2.insert({val - 1, i});
+        int lw1 =
+            lower_bound(comp.begin(), comp.end(), nums[0]) - comp.begin() + 1;
+        int lw2 =
+            lower_bound(comp.begin(), comp.end(), nums[1]) - comp.begin() + 1;
+        update(lw1, m, bits1);
+        update(lw2, m, bits2);
+
+        for (int i = 2; i < n; i++) {
+            int idx = lower_bound(comp.begin(), comp.end(), nums[i]) -
+                      comp.begin() + 1;
+
+            int greater1 = arr1.size() - query(idx, bits1);
+            int greater2 = arr2.size() - query(idx, bits2);
+
+            if (greater1 > greater2) {
+                arr1.push_back(nums[i]);
+                update(idx, m, bits1);
+            } else if (greater1 < greater2) {
+                arr2.push_back(nums[i]);
+                update(idx, m, bits2);
             } else {
-                if (arr1.size() > arr2.size()) {
-                    arr2.push_back(val - 1);
-                    st2.insert({val - 1, i});
+                if (arr1.size() <= arr2.size()) {
+                    arr1.push_back(nums[i]);
+                    update(idx, m, bits1);
                 } else {
-                    arr1.push_back(val - 1);
-                    st1.insert({val - 1, i});
+                    arr2.push_back(nums[i]);
+                    update(idx, m, bits2);
                 }
             }
         }
-        for(int &num:arr1){
-            ans.push_back(num);
-        }
-        for(int &num:arr2){
-            ans.push_back(num);
-        }
+        vector<int> ans;
+        ans.insert(ans.end(), arr1.begin(), arr1.end());
+        ans.insert(ans.end(), arr2.begin(), arr2.end());
         return ans;
     }
 };
