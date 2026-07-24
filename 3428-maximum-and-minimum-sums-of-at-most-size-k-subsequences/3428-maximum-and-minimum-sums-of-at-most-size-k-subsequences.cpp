15e3d49193ab9fcb4dc8e7963766b527ee6dc01d
Ankit Singh <126193019+Ankiitsingh21@users.noreply.github.com>
2026-07-25 05:07:50 +0530

    Time: 275 ms (50.78%), Space: 276.6 MB (14.06%) - LeetHub

--- 3428-maximum-and-minimum-sums-of-at-most-size-k-subsequences/3428-maximum-and-minimum-sums-of-at-most-size-k-subsequences.cpp
+++ 3428-maximum-and-minimum-sums-of-at-most-size-k-subsequences/3428-maximum-and-minimum-sums-of-at-most-size-k-subsequences.cpp
@@ -1,67 +1,34 @@
 class Solution {
-    static const int MOD = 1e9 + 7;
-
-    void buildNCR(vector<vector<long long>> &nCr, int n, int k) {
-
-        nCr.assign(n + 1, vector<long long>(k, 0));
-
-        // C(i,0) = 1
-        for (int i = 0; i <= n; i++)
-            nCr[i][0] = 1;
-
-        // Pascal's Formula
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j < k && j <= i; j++) {
-                nCr[i][j] = (nCr[i - 1][j - 1] + nCr[i - 1][j]) % MOD;
-            }
-        }
-    }
-
-    void buildWays(vector<long long> &ways,
-                   const vector<vector<long long>> &nCr,
-                   int n,
-                   int k) {
-
-        ways.assign(n + 1, 0);
-
-        for (int i = 0; i <= n; i++) {
-
-            long long sum = 0;
-
-            for (int j = 0; j < k && j <= i; j++) {
-                sum = (sum + nCr[i][j]) % MOD;
-            }
-
-            ways[i] = sum;
-        }
-    }
-
+    const int MOD=1e9+7;
 public:
     int minMaxSums(vector<int>& nums, int k) {
-
         int n = nums.size();
-
         sort(nums.begin(), nums.end());
-
-        vector<vector<long long>> nCr;
-        buildNCR(nCr, n, k);
-
-        vector<long long> ways;
-        buildWays(ways, nCr, n, k);
-
-        long long ans = 0;
-
+        vector<vector<long long>> ncr(n + 1, vector<long long>(k, 0));
+        for (int i = 0; i <= n; i++) {
+            ncr[i][0] = 1;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j < k && j <= i; j++) {
+                ncr[i][j] = (ncr[i - 1][j - 1] + ncr[i - 1][j]) % MOD;
+            }
+        }
+        vector<long long> ways(n + 1, 0);
+        for (int i = 0; i <= n; i++) {
+            long long sum = 0;
+            for (int j = 0; j < k && j <= i; j++) {
+                sum = (sum + ncr[i][j]) % MOD;
+            }
+            ways[i] = sum;
+        }
+        int ans = 0;
         for (int i = 0; i < n; i++) {
-
             int left = i;
             int right = n - i - 1;
-
-            long long cntMax = ways[left];
-            long long cntMin = ways[right];
-
-            ans = (ans + 1LL * nums[i] * ((cntMax + cntMin) % MOD)) % MOD;
+            int cntL = ways[left];
+            int cntR = ways[right];
+            ans = (ans + (1LL * nums[i] * (cntL + cntR)) % MOD) % MOD;
         }
-
         return ans;
     }
 };
