d8b19deb0c6b02d1cb9cfec89acaacd579b94383
Ankit Singh <126193019+Ankiitsingh21@users.noreply.github.com>
2026-07-24 08:06:16 +0530

    Time: 0 ms (100%), Space: 198.8 MB (59.63%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
         int n = nums.size();
-
-        if (n == 1) return 1;
-        if (n == 2) return 2;
-
-        int bits = 32 - __builtin_clz(n);   // bit width of n
-        return 1 << bits;
+        if (n == 1 || n == 2)
+            return n;
+        int cnt = 0;
+        while (n > 0) {
+            cnt++;
+            n >>= 1;
+        }
+        return 1 << cnt;
     }
 };
