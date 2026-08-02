2209f699a1c65b0f841b75d8e8e2661c4a158419
Ankit Singh <126193019+Ankiitsingh21@users.noreply.github.com>
2026-08-02 23:14:32 +0530

    Time: 19 ms (14.03%), Space: 19.5 MB (27.79%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,22 +1,26 @@
 class Solution {
-    int solve(int i,int j,vector<int> &nums,vector<vector<int>>&dp,int turn){
-        if(i>j) return 0;
+    int solve(int i, int j, vector<int>& nums, vector<vector<int>>& dp,
+              int turn) {
+        if (i > j)
+            return 0;
 
-        if(dp[i][j]!=INT_MIN) return dp[i][j];
-        if(turn ==0) {
-            int takeL=nums[i]+solve(i+1,j,nums,dp,!turn);
-            int takeR=nums[j]+solve(i,j-1,nums,dp,!turn);
-            return dp[i][j]=max(takeL,takeR);
-        }else{
-            int takeL=-nums[i]+solve(i+1,j,nums,dp,!turn);
-            int takeR=-nums[j]+solve(i,j-1,nums,dp,!turn);
-            return dp[i][j]=max(takeL,takeR);
+        if (dp[i][j] != INT_MIN)
+            return dp[i][j];
+        if (turn == 0) {
+            int takeL = nums[i] + solve(i + 1, j, nums, dp, !turn);
+            int takeR = nums[j] + solve(i, j - 1, nums, dp, !turn);
+            return dp[i][j] = max(takeL, takeR);
+        } else {
+            int takeL = -nums[i] + solve(i + 1, j, nums, dp, !turn);
+            int takeR = -nums[j] + solve(i, j - 1, nums, dp, !turn);
+            return dp[i][j] = max(takeL, takeR);
         }
     }
+
 public:
     bool stoneGame(vector<int>& piles) {
-        int n=piles.size();
-        vector<vector<int>> arr(n,vector<int>(n,INT_MIN));
-        return solve(0,n-1,piles,arr,0)>0;
+        int n = piles.size();
+        vector<vector<int>> arr(n, vector<int>(n, INT_MIN));
+        return solve(0, n - 1, piles, arr, 0) > 0;
     }
 };
